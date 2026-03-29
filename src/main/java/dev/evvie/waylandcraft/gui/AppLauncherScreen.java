7eb5c7d30cd2599a4abc9a6ffde36ce18e96b3f6
EVV1E <fabillo552@gmail.com>
2026-03-29 20:31:52 +0200

    App launcher scroll bar and small additions

--- src/main/java/dev/evvie/waylandcraft/gui/AppLauncherScreen.java
+++ src/main/java/dev/evvie/waylandcraft/gui/AppLauncherScreen.java
@@ -3,11 +3,13 @@ package dev.evvie.waylandcraft.gui;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
+import org.joml.Random;
 
 import dev.evvie.waylandcraft.WaylandCraft;
 import dev.evvie.waylandcraft.desktop.DesktopEntry;
 import dev.evvie.waylandcraft.render.RenderUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -15,16 +17,17 @@ import net.minecraft.client.gui.components.AbstractContainerWidget;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.navigation.CommonInputs;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
 
 public class AppLauncherScreen extends Screen {
 	
 	private WaylandCraft wlc;
-	private static final ResourceLocation SLOT_THINGY = new ResourceLocation(WaylandCraft.MOD_ID, "textures/gui/slot_thingy.png");
-	private static final ResourceLocation SLOT_THINGY_SELECTED = new ResourceLocation(WaylandCraft.MOD_ID, "textures/gui/slot_thingy_selected_overlay.png");
+	private AppList list;
 	
 	public AppLauncherScreen(WaylandCraft wlc) {
 		super(Component.literal("App Launcher"));
@@ -34,9 +37,14 @@ public class AppLauncherScreen extends Screen {
 	
 	@Override
 	protected void init() {
-		int listWidth = 210;
+		int listWidth = AppList.ELEMENT_WIDTH;
 		int listHeight = 170;
-		this.addRenderableWidget(new AppList(wlc, width / 2 - listWidth / 2, height / 2 - listHeight / 2, listWidth, listHeight));
+		list = new AppList(this, width / 2 - listWidth / 2, height / 2 - listHeight / 2, listWidth, listHeight);
+		this.addRenderableWidget(this.list);
+		
+		Random random = new Random();
+		List<DesktopEntry> entries = wlc.xdgManager.entries().stream().filter((e) -> e.visible && random.nextFloat() < 0.2f).toList();
+		list.setEntries(entries);
 	}
 	
 	@Override
@@ -44,40 +52,49 @@ public class AppLauncherScreen extends Screen {
 		return false;
 	}
 	
+	public void launch(DesktopEntry entry) {
+		this.onClose();
+	}
+	
 	private static class AppList extends AbstractContainerWidget {
 		
-		private static final int SLOT_GAPS = 4;
+		private static final ResourceLocation SCROLLER_SPRITE = new ResourceLocation("widget/scroller");
+		private static final ResourceLocation SCROLLER_BACKGROUND_SPRITE = new ResourceLocation("widget/scroller_background");
+		
+		private static final int SLOT_GAPS = 2;
 		private static final int ELEMENT_WIDTH = 200 + 2;
 		private static final int ELEMENT_HEIGHT = 32 + 2;
 		
-		private WaylandCraft wlc;
+		private AppLauncherScreen screen;
 		private ArrayList<AppWidget> children = new ArrayList<AppWidget>();
 		
 		private int maxScroll = 0;
 		private int scroll = 0;
 		private int contentHeight = 0;
 		
-		public AppList(WaylandCraft wlc, int x, int y, int width, int height) {
+		public AppList(AppLauncherScreen screen, int x, int y, int width, int height) {
 			super(x, y, width, height, Component.literal("App List"));
-			this.wlc = wlc;
-			
-			List<DesktopEntry> allEntries = wlc.xdgManager.entries();
-			Random random = new Random();
-			
-			int count = 10;
-			for(int i = 0; i < count; i++) {
-				DesktopEntry entry = null;
-				while(!(entry != null && entry.visible)) entry = allEntries.get(random.nextInt(allEntries.size()));
-				
-				children.add(new AppWidget(entry));
+			this.screen = screen;
+		}
+		
+		public void setEntries(List<DesktopEntry> entries) {
+			children.clear();
+			for(DesktopEntry entry : entries) {
+				children.add(new AppWidget(screen, entry));
 			}
-			
+			scroll = 0;
 			rearrangeChildren();
 		}
 		
 		private void rearrangeChildren() {
+			contentHeight = children.size() * (ELEMENT_HEIGHT + SLOT_GAPS) - SLOT_GAPS;
+			maxScroll = Math.max(contentHeight - height, 0);
+			
+			if(scroll < 0) scroll = 0;
+			if(scroll > maxScroll) scroll = maxScroll;
+			
 			int x = getX();
-			int y = getY() + 4;
+			int y = getY();
 			y -= scroll;
 			
 			for(int i = 0; i < children.size(); i++) {
@@ -85,21 +102,19 @@ public class AppLauncherScreen extends Screen {
 				widget.setRectangle(ELEMENT_WIDTH, ELEMENT_HEIGHT, x + width / 2 - ELEMENT_WIDTH / 2, y + i * (ELEMENT_HEIGHT + SLOT_GAPS));
 			}
 			
-			contentHeight = 4 + children.size() * (ELEMENT_HEIGHT + SLOT_GAPS) - SLOT_GAPS;
-			maxScroll = Math.max(contentHeight - height, 0);
 		}
 		
 		private void scrollTo(AppWidget widget) {
-			boolean topCondition = widget.getY() >= getY() + 4;
+			boolean topCondition = widget.getY() >= getY();
 			boolean bottomCondition = widget.getBottom() <= getBottom();
 			if(topCondition && bottomCondition) {
 				/* Widget already in view */
 				return;
 			}
 			
-			int top = children.get(0).getY() - 4;
+			int top = children.get(0).getY();
 			int bottomScroll = widget.getBottom() - top - height;
-			int topScroll = widget.getY() - top - 4;
+			int topScroll = widget.getY() - top;
 			
 			if(!bottomCondition) scroll = bottomScroll;
 			else scroll = topScroll;
@@ -132,7 +147,8 @@ public class AppLauncherScreen extends Screen {
 			int width = getWidth();
 			int height = getHeight();
 			
-			context.fill(x, y, x + width, y + height, Color.black.getRGB());
+			context.renderOutline(x - 1, y - 1, width + 2, height + 2, Color.black.getRGB());
+			context.renderOutline(x - 2, y - 2, width + 4, height + 4, Color.black.getRGB());
 			
 			context.enableScissor(x, y, x + width, y + height);
 			
@@ -141,6 +157,14 @@ public class AppLauncherScreen extends Screen {
 			}
 			
 			context.disableScissor();
+			
+			int scrollerHeight = height + 4;
+			
+			int scrollerSize = Math.round(height / (float) contentHeight * scrollerHeight);
+			int scrollerPos = Math.round(scroll / (float) contentHeight * scrollerHeight);
+			
+			context.blitSprite(SCROLLER_BACKGROUND_SPRITE, x + width + 8, y - 2, 6, scrollerHeight);
+			context.blitSprite(SCROLLER_SPRITE, x + width + 8, y - 2 + scrollerPos, 6, scrollerSize);
 		}
 		
 		@Override
@@ -156,12 +180,17 @@ public class AppLauncherScreen extends Screen {
 	
 	private static class AppWidget extends AbstractWidget {
 		
+		private static final ResourceLocation SLOT_THINGY = new ResourceLocation(WaylandCraft.MOD_ID, "textures/gui/slot_thingy.png");
+		private static final ResourceLocation SLOT_THINGY_SELECTED = new ResourceLocation(WaylandCraft.MOD_ID, "textures/gui/slot_thingy_selected_overlay.png");
+		
 		public final DesktopEntry entry;
+		private AppLauncherScreen screen;
 		private Font font;
 		
-		public AppWidget(DesktopEntry entry) {
+		public AppWidget(AppLauncherScreen screen, DesktopEntry entry) {
 			super(0, 0, 0, 0, Component.literal(getTitle(entry)));
 			this.entry = entry;
+			this.screen = screen;
 			this.font = Minecraft.getInstance().font;
 		}
 		
@@ -175,22 +204,42 @@ public class AppLauncherScreen extends Screen {
 			int y = getY() + 1;
 			int width = getWidth() - 2;
 			int height = getHeight() - 2;
-			boolean selected = isHoveredOrFocused();
+			boolean selected = isFocused();
 			
 			RenderUtils.blit(context, SLOT_THINGY, x, y, width, height);
 			if(selected) RenderUtils.blit(context, SLOT_THINGY_SELECTED, x - 1, y - 1, width + 2, height + 2);
 			
 			int iconSize = entry.icon != null ? height - 10 : 0;
 			
+			MutableComponent text = Component.literal(getTitle(entry));
+			if(isHoveredOrFocused()) text = text.withStyle(ChatFormatting.UNDERLINE);
+			
 			context.enableScissor(x + 4, y + 4, x + width - 4, y + height - 4);
 			if(entry.icon != null) RenderUtils.blit(context, entry.icon, x + 5, y + 5, iconSize, iconSize);
-			context.drawString(font, getTitle(entry), x + 5 + iconSize + 5, y + height / 2 - font.lineHeight / 2, Color.white.getRGB());
+			context.drawString(font, text, x + 5 + iconSize + 5, y + height / 2 - font.lineHeight / 2, Color.white.getRGB());
 			context.disableScissor();
 			
 			if(selected) {
 				context.renderOutline(x - 1, y - 1, width + 2, height + 2, Color.white.getRGB());
-				context.fill(x + 4, y + 4, x + width - 4, y + height - 4, 1, FastColor.ARGB32.color(128, Color.black.getRGB()));
+				context.fill(x + 4, y + 4, x + width - 4, y + height - 4, 1, FastColor.ARGB32.color(64, Color.black.getRGB()));
 			}
+		}
+		
+		public void launch() {
+			screen.launch(entry);
+		}
+		
+		@Override
+		public void onClick(double mouseX, double mouseY) {
+			launch();
+		}
+		
+		@Override
+		public boolean keyPressed(int key, int scancode, int modifiers) {
+			if(!visible || !active) return false;
+			if(!CommonInputs.selected(key)) return false;
+			launch();
+			return true;
 		}
 		
 		@Override
